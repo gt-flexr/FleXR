22f61e863840574fd5919c337ef29c635a672df4
Jin Heo <993jin@gmail.com>
2020-07-17 13:39:26 -0400

    Update EGL example with OGL loading

--- tests/egl0.cc
+++ tests/egl0.cc
@@ -1,23 +1,38 @@
-  #include <EGL/egl.h>
+#include <opencv4/opencv2/opencv.hpp>
+#include <EGL/egl.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <stdio.h>
 
-  static const EGLint configAttribs[] = {
-          EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
-          EGL_BLUE_SIZE, 8,
-          EGL_GREEN_SIZE, 8,
-          EGL_RED_SIZE, 8,
-          EGL_DEPTH_SIZE, 8,
-          EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
-          EGL_NONE
-  };
+using namespace std;
 
-  static const int pbufferWidth = 9;
-  static const int pbufferHeight = 9;
+static const EGLint configAttribs[] = {
+    EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+    EGL_BLUE_SIZE, 8,
+    EGL_GREEN_SIZE, 8,
+    EGL_RED_SIZE, 8,
+    EGL_DEPTH_SIZE, 8,
+    EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+    EGL_NONE};
 
-  static const EGLint pbufferAttribs[] = {
-        EGL_WIDTH, pbufferWidth,
-        EGL_HEIGHT, pbufferHeight,
-        EGL_NONE,
-  };
+static const int pbufferWidth = 1280;
+static const int pbufferHeight = 640;
+static const EGLint pbufferAttribs[] = {
+    EGL_WIDTH, pbufferWidth,
+    EGL_HEIGHT, pbufferHeight,
+    EGL_NONE,
+};
+
+cv::Mat capture()
+{
+  glReadBuffer(GL_FRONT);
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+  unsigned char *pixels = new unsigned char[3 * 1280 * 640];
+  glReadPixels(0, 0, 1280, 640, GL_BGR, GL_UNSIGNED_BYTE, pixels);
+
+  return cv::Mat(640, 1280, CV_8UC3, pixels);
+}
 
 int main(int argc, char *argv[])
 {
@@ -25,6 +40,9 @@ int main(int argc, char *argv[])
   EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 
   EGLint major, minor;
+  EGLint test;
+  GLubyte *data = (GLubyte*)malloc(1280 * 640 * 3);
+  //GLubyte *data = new GLubyte[1280*640*3];
 
   eglInitialize(eglDpy, &major, &minor);
 
@@ -48,6 +66,17 @@ int main(int argc, char *argv[])
   eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx);
 
   // from now on use your OpenGL context
+  glClearColor(1.0, 0.0, 0.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  eglSwapBuffers(eglDpy, eglSurf);
+  //glReadPixels(0, 0, 1280, 640, GL_RGB, GL_UNSIGNED_BYTE, data);
+  cv::Mat cv_data = capture();
+  cv::imwrite("result.jpg", cv_data);
+
+  eglQuerySurface(eglDpy, eglSurf, EGL_WIDTH, &test);
+  cout << "Surface WIDTH: " << test << endl;
+  eglQuerySurface(eglDpy, eglSurf, EGL_HEIGHT, &test);
+  cout << "Surface HEIGHT: " << test << endl;
 
   // 6. Terminate EGL when finished
   eglTerminate(eglDpy);
