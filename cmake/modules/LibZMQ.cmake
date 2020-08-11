9afe60273b0f0331f022fe1bc1ebfcd3e2719285
Jin Heo <993jin@gmail.com>
2020-08-10 22:58:16 -0400

    Add libzmq to the build system

---
+++ cmake/modules/LibZMQ.cmake
@@ -0,0 +1,12 @@
+macro(use_libzmq)
+  pkg_search_module(LIBZMQ REQUIRED libzmq)
+  if(LIBZMQ_FOUND)
+    message(STATUS "[LibZMQ Details]")
+    message("\t LIBZMQ_INCLUDE_DIRS: ${LIBZMQ_INCLUDE_DIRS}")
+    message("\t LIBZMQ_LDFLAGS: ${LIBZMQ_LDFLAGS}")
+
+    include_directories(${LIBZMQ_INCLUDE_DIRS})
+    list(APPEND MXRE_CXX_FLAGS ${LIBZMQ_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${LIBZMQ_LDFLAGS})
+  endif(LIBZMQ_FOUND)
+endmacro()
