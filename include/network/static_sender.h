53065448a3d16167c3c209f56b753060ff379d61
Jin Heo <993jin@gmail.com>
2020-08-20 22:27:03 -0400

    Add and Test static type sender/receiver

---
+++ include/network/static_sender.h
@@ -0,0 +1,78 @@
+#ifndef __MXRE_STATIC_SENDER__
+#define __MXRE_STATIC_SENDER__
+
+#include <bits/stdc++.h>
+#include <raft>
+#include <zmq.h>
+#include <cv.hpp>
+#include "defs.h"
+#include "message_sender.h"
+
+namespace mxre
+{
+  namespace pipeline
+  {
+    namespace network
+    {
+      /* Class Deifinition */
+      template<typename IN_T>
+      class StaticSender : public MessageSender
+      {
+        private:
+          bool isVector;
+        public:
+          StaticSender(std::string addr="localhost", int port=5555, bool isVector=false);
+          virtual raft::kstatus run();
+      };
+
+
+      /* Constructor() */
+      template<typename IN_T>
+      StaticSender<IN_T>::StaticSender(std::string addr, int port, bool isVector) : MessageSender(addr, port) {
+        this->isVector = isVector;
+        if(isVector)
+          input.addPort<std::vector<IN_T>>("in_data");
+        else
+          input.addPort<IN_T>("in_data");
+      }
+
+
+      /* Run() */
+      template<typename IN_T>
+      raft::kstatus StaticSender<IN_T>::run() {
+        debug_print("[StaticSender] isVector(%d) START", isVector);
+        uint numElem, elemSize;
+        void *data;
+        char ackMsg[4];
+
+
+        if(isVector) {
+          auto &inData( input["in_data"].template peek<std::vector<IN_T>>() );
+          numElem = inData.size();
+          elemSize = sizeof(IN_T);
+          data = inData.data();
+        }
+        else {
+          auto &inData( input["in_data"].template peek<IN_T>() );
+          numElem = 1;
+          elemSize = sizeof(inData);
+          data = &inData;
+        }
+        debug_print("numElem (%d), elemSize (%d)", numElem, elemSize);
+
+        // 1. Send the num of data & data
+        zmq_send(sock, &numElem, sizeof(uint), ZMQ_SNDMORE);
+        zmq_send(sock, data, numElem * elemSize, 0);
+
+        // 2. Recv ACK
+        zmq_recv(sock, ackMsg, 4, 0);
+
+        input["in_data"].recycle(1);
+        return raft::proceed;
+      }
+    } // namespace network
+  }   // namespace pipeline
+} // namespace mxre
+
+#endif
+
