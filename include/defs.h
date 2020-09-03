cedd60662cd98b155ad34a407b2cda484c12c141
Jin Heo <993jin@gmail.com>
2020-09-03 00:16:39 -0400

    Update debug flag with cmake

--- include/defs.h
+++ include/defs.h
@@ -9,10 +9,10 @@
 
 #define SCALE_FACTOR 10
 
-#define __DEBUG__ 1
 // color-print reference: http://web.theurbanpenguin.com/adding-color-to-your-output-from-c/
-#define debug_print(...) do { if (__DEBUG__) \
+#define debug_print(...) do { if (__DEBUG__) { \
                               fprintf(stderr, "\033[1;31m[DEBUG] \033[0;32m[FUNC] %s \033[0m", __PRETTY_FUNCTION__); \
                               fprintf(stderr, __VA_ARGS__); \
-                              fprintf(stderr, "\n"); } while (0)
+                              fprintf(stderr, "\n"); } \
+                            } while (0)
 #endif
