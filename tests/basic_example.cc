ad66e047cbcd70e061677a5474e27cbb3146a0d7
Jin Heo <993jin@gmail.com>
2020-09-11 13:23:21 -0400

    Update basic example with cv display

--- tests/basic_example.cc
+++ tests/basic_example.cc
@@ -4,28 +4,6 @@
 
 using namespace std;
 
-class TestSink : public raft::kernel {
-  public:
-  TestSink(): raft::kernel() {
-    input.addPort<cv::Mat>("in_frame");
-    input.addPort<clock_t>("in_timestamp");
-  }
-
-  virtual raft::kstatus run() {
-    auto frame = input["in_frame"].peek<cv::Mat>();
-    auto st = input["in_timestamp"].peek<clock_t>();
-
-    cv::imshow("TestSink Received", frame);
-    int inKey = cv::waitKey(100) & 0xFF;
-    printf("[TestSink] st(%ld) \n", st);
-
-    input["in_frame"].recycle();
-    input["in_timestamp"].recycle();
-
-    return raft::proceed;
-  }
-};
-
 int main(int argc, char const *argv[])
 {
   cv::CommandLineParser parser(argc, argv,
@@ -88,7 +66,7 @@ int main(int argc, char const *argv[])
       if(roiRect.width == 0 || roiRect.height == 0)
         i--;
       else
-       objTracker.registerObject(frame, roiRect);
+        objTracker.registerObject(frame, roiRect);
     }
   }
   objTracker.printRegisteredObjects();
@@ -96,14 +74,9 @@ int main(int argc, char const *argv[])
 
   mxre::pipeline::input_srcs::Camera cam(camera_no);
   mxre::pipeline::ctx_understanding::ObjectDetector objDetector(objTracker.getRegisteredObjects(), orb, matcher);
-  printf("%f %f %f\n",
-   cam.getIntrinsic().at<double>(0, 0),
-   cam.getIntrinsic().at<double>(0, 1),
-   cam.getIntrinsic().at<double>(0, 2)
-  );
   mxre::pipeline::contextualizing::ObjectCtxExtractor objCtxExtractor(cam.getIntrinsic(), cam.getDistCoeffs());
   mxre::pipeline::rendering::ObjectRenderer objRenderer;
-  TestSink sink;
+  mxre::pipeline::output_sinks::CVDisplay cvDisplay;
   //mxre::pipeline::rendering::ObjectOverlayer objOverlayer;
   //mxre::pipeline::output_sink::Display disp;
 
@@ -111,22 +84,19 @@ int main(int argc, char const *argv[])
 
   // cam - obj detector
   pipeline += cam["out_frame"] >> objDetector["in_frame"];
-  pipeline += cam["out_timestamp"] >> objDetector["in_timestamp"];
 
   // obj detector - obj ctx extractor
   pipeline += objDetector["out_frame"] >> objCtxExtractor["in_frame"];
-  pipeline += objDetector["out_timestamp"] >> objCtxExtractor["in_timestamp"];
   pipeline += objDetector["out_obj_info"] >> objCtxExtractor["in_obj_info"];
 
   // obj ctx extractor - obj renderer
   pipeline += objCtxExtractor["out_frame"] >> objRenderer["in_frame"];
-  pipeline += objCtxExtractor["out_timestamp"] >> objRenderer["in_timestamp"];
   pipeline += objCtxExtractor["out_obj_context"] >> objRenderer["in_obj_context"];
 
   // obj renderer - test sink
-  pipeline += objRenderer["out_frame"] >> sink["in_frame"];
-  pipeline += objRenderer["out_timestamp"] >> sink["in_timestamp"];
+  pipeline += objRenderer["out_frame"] >> cvDisplay["in_frame"];
 
   pipeline.exe();
- return 0;
+  return 0;
 }
+
