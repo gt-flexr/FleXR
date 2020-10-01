77878c84750bbf8d8f83a048dbae2bd42ac08b95
Jin Heo <993jin@gmail.com>
2020-09-30 22:47:01 -0400

    Update object_ctx_extractor with glm and 3D models

--- src/object_ctx_extractor.cc
+++ src/object_ctx_extractor.cc
@@ -1,5 +1,5 @@
-#include <include/object_ctx_extractor.h>
-#include <include/gl_types.h>
+#include <object_ctx_extractor.h>
+#include <gl/gl_types.h>
 
 namespace mxre
 {
@@ -17,7 +17,12 @@ namespace mxre
 
         // output..
         output.addPort<mxre::cv_units::Mat>("out_frame");
-        output.addPort<std::vector<mxre::gltypes::ObjectContext>>("out_obj_context");
+        output.addPort<std::vector<mxre::gl::ObjectContext>>("out_obj_context");
+
+#ifdef __PROFILE__
+        input.addPort<FrameStamp>("frame_stamp");
+        output.addPort<FrameStamp>("frame_stamp");
+#endif
       }
 
       ObjectCtxExtractor::~ObjectCtxExtractor() {}
@@ -36,32 +41,45 @@ namespace mxre
         // set outputs
         auto &out_frame( output["out_frame"].template allocate<mxre::cv_units::Mat>() );
         auto &out_obj_context(
-            output["out_obj_context"].template allocate<std::vector<mxre::gltypes::ObjectContext>>() );
+            output["out_obj_context"].template allocate<std::vector<mxre::gl::ObjectContext>>() );
 
-        std::vector<mxre::gltypes::ObjectContext> objCtxVec;
+        std::vector<mxre::gl::ObjectContext> objCtxVec;
         std::vector<mxre::cv_units::ObjectInfo>::iterator objIter;
         for (objIter = objInfoVec.begin(); objIter != objInfoVec.end(); ++objIter)
         {
           if (objIter->location2D.size() == 4 && objIter->isDetected)
           {
             cv::Mat rvec, tvec;
-            //printf("camIntrinsic %f %f %f \n", camIntrinsic.at<double>(0, 0), camIntrinsic.at<double>(0, 1),
-            //    camIntrinsic.at<double>(0, 2));
 
             cv::solvePnPRansac(objIter->rect3D, objIter->location2D, camIntrinsic, camDistCoeffs, rvec, tvec);
+
+            mxre::gl::ObjectContext objCtx;
+            objCtx.index = objIter->index;
+            float transX = (tvec.at<double>(0, 0) * 2) / WIDTH;
+            float transY = (tvec.at<double>(0, 1) * 2) / HEIGHT;
+            float transZ = tvec.at<double>(0, 2) / WIDTH;
+
+            float rotX = rvec.at<double>(0, 0);
+            float rotY = rvec.at<double>(0, 1);
+            float rotZ = rvec.at<double>(0, 2);
+
+            // Convert the OCV coordinate system into the OGL coordinate system
+            objCtx.rvec.x = -rotY;   objCtx.rvec.y = -rotZ;   objCtx.rvec.z = rotX;
+            objCtx.tvec.x = -transY; objCtx.tvec.y = -transZ; objCtx.tvec.z = transX;
+
+            //def draw_axis(img, R, t, K):
+            //  rotV, _ = cv2.Rodrigues(R)
+            //    points = np.float32([[100, 0, 0], [0, 100, 0], [0, 0, 100], [0, 0, 0]]).reshape(-1, 3)
+            //    axisPoints, _ = cv2.projectPoints(points, rotV, t, K, (0, 0, 0, 0))
+            //    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[0].ravel()), (255,0,0), 3)
+            //    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[1].ravel()), (0,255,0), 3)
+            //    img = cv2.line(img, tuple(axisPoints[3].ravel()), tuple(axisPoints[2].ravel()), (0,0,255), 3)
+            //    return img
+
+            /*
             cv::Mat rvecMat;
             cv::Rodrigues(rvec, rvecMat);
             cv::Mat cvViewMat(4, 4, CV_64F);
-            /*
-            for (unsigned int row = 0; row < 3; ++row) {
-              for (unsigned int col = 0; col < 3; ++col) {
-                cvViewMat.at<double>(row, col) = rvecMat.at<double>(row, col);
-                printf("%f ", rvecMat.at<double>(row, col));
-              }
-              printf("\n");
-            }
-            printf("\n");
-            */
             cvViewMat.at<double>(3, 3) = 1.0f;
 
             // opencv x-y-z == opengl x-z-y
@@ -71,10 +89,7 @@ namespace mxre
             yzSwapMat.at<double>(2, 2) = -1.0f; // invert the z axis
             yzSwapMat.at<double>(3, 3) = 1.0f;
             cvViewMat = yzSwapMat * cvViewMat;
-            cv::transpose(cvViewMat, cvViewMat);
-
-            mxre::gltypes::ObjectContext objCtx;
-            objCtx.index = objIter->index;
+            cv::transpose(cvViewMat, cvViewMat); */
 
             /*
             for (unsigned int row = 0; row < 4; ++row) {
@@ -87,14 +102,7 @@ namespace mxre
             printf("\n");
             */
 
-            float transX = (tvec.at<double>(0, 0) * 2) / WIDTH;
-            float transY = (tvec.at<double>(0, 1) * 2) / WIDTH;
-            float transZ = tvec.at<double>(0, 2) / WIDTH;
-            //printf("[Obj Position] x(%lf), y(%lf), z(%lf) \n", transX, transY, transZ);
-            //printf("[Obj Position with SCALE_FACTOR] x(%lf), y(%lf), z(%lf) \n",
-            //       transX * SCALE_FACTOR, transY * SCALE_FACTOR, -SCALE_FACTOR - transZ * SCALE_FACTOR);
-            //printf("\n");
-            objCtx.modelMat.translate(transX * SCALE_FACTOR, transY * SCALE_FACTOR, -SCALE_FACTOR - transZ * SCALE_FACTOR);
+            // objCtx.modelMat.translate(transX * SCALE_FACTOR, transY * SCALE_FACTOR, -SCALE_FACTOR - transZ * SCALE_FACTOR);
 
             objCtxVec.push_back(objCtx);
           }
@@ -108,6 +116,12 @@ namespace mxre
 #ifdef __PROFILE__
         TimeVal end = getNow();
         debug_print("Exe Time: %lfms", getExeTime(end, start));
+
+        auto &inFrameStamp( input["frame_stamp"].peek<FrameStamp>() );
+        auto &outFrameStamp( output["frame_stamp"].allocate<FrameStamp>() );
+        outFrameStamp = inFrameStamp;
+        input["frame_stamp"].recycle();
+        output["frame_stamp"].send();
 #endif
 
         output["out_frame"].send();
