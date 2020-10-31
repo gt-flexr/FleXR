edb89e504fa8e7ddaf4cb3a7929c63daa954ff08
Jin Heo <993jin@gmail.com>
2020-10-31 16:13:11 -0400

    Update CMakeLists and Add pc for installation

--- cmake/modules/OpenGL.cmake
+++ cmake/modules/OpenGL.cmake
@@ -1,44 +1,41 @@
 # https://cmake.org/cmake/help/v3.10/module/FindOpenGL.html
 
 macro(use_opengl)
-  include(FindOpenGL)
-  find_package(GLUT REQUIRED)
-  find_package(GLEW REQUIRED)
-  pkg_search_module(GLM REQUIRED glm)
-  find_package(ASSIMP REQUIRED)
-
-  message(STATUS "[OpenGL Details]")
-  if (OPENGL_FOUND)
-    message("\t OPENGL_INCLUDE_DIR: ${OPENGL_INCLUDE_DIR}")
-    message("\t OPENGL_LIBRARY: ${OPENGL_LIBRARY}")
-    message("\t OPENGL_EGL_INCLUDE_DIR: ${OPENGL_EGL_INCLUDE_DIR}")
-    message("\t OPENGL_egl_LIBRARY: ${OPENGL_egl_LIBRARY}")
-
-    include_directories(${OPENGL_INCLUDE_DIR})
-    include_directories(${OPENGL_EGL_INCLUDE_DIR})
-    list(APPEND MXRE_LINKER_LIBS ${OPENGL_LIBRARY} ${OPENGL_egl_LIBRARY})
-    list(APPEND MXRE_LINKER_FLAGS -lGL -lGLU -lm)
-  endif ()
-
-  if(GLUT_FOUND)
-    message("\t GLUT_LIBRARIES: ${GLUT_LIBRARIES}")
-    list(APPEND MXRE_LINKER_LIBS ${GLUT_LIBRARIES})
-    list(APPEND MXRE_LINKER_FLAGS -lglut)
+  pkg_search_module(OGL REQUIRED gl)
+  if(OGL_FOUND)
+    list(APPEND MXRE_CXX_FLAGS ${OGL_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${OGL_LDFLAGS})
   endif()
 
+
+  pkg_search_module(EGL REQUIRED egl)
+  if(EGL_FOUND)
+    list(APPEND MXRE_CXX_FLAGS ${EGL_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${EGL_LDFLAGS})
+
+  endif()
+
+
+  pkg_search_module(GLEW REQUIRED glew)
   if(GLEW_FOUND)
-    message("\t GLEW_LIBRARIES: ${GLEW_LIBRARIES}")
-    list(APPEND MXRE_LINKER_LIBS ${GLEW_LIBRARIES})
+    #message("\t GLEW_LIBRARIES: ${GLEW_LIBRARIES}")
+    #list(APPEND MXRE_LINKER_LIBS ${GLEW_LIBRARIES})
+    list(APPEND MXRE_CXX_FLAGS ${GLEW_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${GLEW_LDFLAGS})
   endif()
 
+
+  pkg_search_module(GLM REQUIRED glm)
   if(GLM_FOUND)
-    message("\t GLM_LIBRARIES: ${GLM_LIBRARIES}")
-    list(APPEND MXRE_LINKER_LIBS ${GLM_LIBRARIES})
+    list(APPEND MXRE_CXX_FLAGS ${GLM_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${GLM_LDFLAGS})
   endif()
 
+
+  pkg_search_module(ASSIMP REQUIRED assimp)
   if(ASSIMP_FOUND)
-    message("\t ASSIMP_LIBRARIES: ${ASSIMP_LIBRARIES}")
-    list(APPEND MXRE_LINKER_LIBS ${ASSIMP_LIBRARIES})
+    list(APPEND MXRE_CXX_FLAGS ${ASSIMP_CFLAGS_OTHER})
+    list(APPEND MXRE_LINKER_FLAGS ${ASSIMP_LDFLAGS})
   endif()
 
 endmacro()
