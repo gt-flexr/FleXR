8e985c37e827c80fdb5b97d9052b59dd1b079300
Jin Heo <993jin@gmail.com>
2020-11-13 01:36:25 -0500

    Update fulloff examples and network module tests

--- examples/modules/network_cvmat_client.cc
+++ examples/modules/network_cvmat_client.cc
@@ -17,32 +17,32 @@ class TestSrc : public raft::kernel {
   TestSrc(bool isVector=false, int numMat=1): raft::kernel(), isVector(isVector), numMat(numMat) {
     cout << "TestSrc isVector: " << isVector << endl;
     if(isVector)
-      output.addPort<vector<mxre::cv_types::Mat>>("out_data");
+      output.addPort<vector<cv::Mat>>("out_data");
     else
-      output.addPort<mxre::cv_types::Mat>("out_data");
+      output.addPort<cv::Mat>("out_data");
   }
 
   virtual raft::kstatus run() {
     printf("[TestSource] run \n");
     if(isVector) {
-      auto &outData( output["out_data"].template allocate<vector<mxre::cv_types::Mat>>() );
+      auto &outData( output["out_data"].template allocate<vector<cv::Mat>>() );
       for(int i = 0; i < numMat; i++) {
-        mxre::cv_types::Mat newMat(16, 2048, CV_32F);
-        newMat.cvMat.at<float>(0, 0) = i;
-        newMat.cvMat.at<float>(0, 1) = i+1;
-        newMat.cvMat.at<float>(0, 2) = i+2;
-        newMat.cvMat.at<float>(0, 3) = i+3;
-        debug_print("%p %p", static_cast<void*>(newMat.data), static_cast<void*>(newMat.cvMat.data));
+        cv::Mat newMat(16, 2048, CV_32F);
+        newMat.at<float>(0, 0) = i;
+        newMat.at<float>(0, 1) = i+1;
+        newMat.at<float>(0, 2) = i+2;
+        newMat.at<float>(0, 3) = i+3;
+        debug_print("%p %p", static_cast<void*>(newMat.data), static_cast<void*>(newMat.data));
         outData.push_back(newMat);
       }
     }
     else {
-      auto &outData( output["out_data"].template allocate<mxre::cv_types::Mat>() );
-      outData = mxre::cv_types::Mat(16, 2048, CV_32F);
-      outData.cvMat.at<float>(0, 0) = 0;
-      outData.cvMat.at<float>(0, 1) = 1;
-      outData.cvMat.at<float>(0, 2) = 2;
-      outData.cvMat.at<float>(0, 3) = 3;
+      auto &outData( output["out_data"].template allocate<cv::Mat>() );
+      outData = cv::Mat(16, 2048, CV_32F);
+      outData.at<float>(0, 0) = 0;
+      outData.at<float>(0, 1) = 1;
+      outData.at<float>(0, 2) = 2;
+      outData.at<float>(0, 3) = 3;
     }
     //sleep(1);
     output["out_data"].send();
@@ -66,3 +66,4 @@ int main(int argc, char const *argv[])
   pipeline.exe();
   return 0;
 }
+
--- examples/modules/roundtrip_client.cc
+++ examples/modules/roundtrip_client.cc
@@ -10,47 +10,6 @@ using namespace std;
 
 void runPipeline(raft::map *pipeline) { pipeline->exe(); }
 
-class TempSrc : public raft::kernel {
-  public:
-  int frame_idx;
-  TempSrc() : raft::kernel() {
-#ifdef __PROFILE__
-    frame_idx=0;
-    output.addPort<mxre::types::FrameStamp>("frame_stamp");
-#endif
-  }
-
-  raft::kstatus run() {
-#ifdef __PROFILE__
-    auto &outFrameStamp( output["frame_stamp"].allocate<mxre::types::FrameStamp>() );
-    outFrameStamp.index = frame_idx++;
-    outFrameStamp.st = getNow();
-    output["frame_stamp"].send();
-#endif
-
-    return raft::proceed;
-  }
-};
-
-
-class TempSink : public raft::kernel {
-  public:
-  TempSink() : raft::kernel() {
-#ifdef __PROFILE__
-    input.addPort<mxre::types::FrameStamp>("frame_stamp");
-#endif
-  }
-
-  raft::kstatus run() {
-#ifdef __PROFILE__
-    auto &inFrameStamp( input["frame_stamp"].peek<mxre::types::FrameStamp>() );
-    input["frame_stamp"].recycle();
-#endif
-    return raft::proceed;
-  }
-};
-
-
 int main(int argc, char const *argv[])
 {
   cv::CommandLineParser parser(argc, argv,
@@ -66,10 +25,6 @@ int main(int argc, char const *argv[])
   mxre::kernels::CVCamera cam(camera_no);
   mxre::kernels::RTPFrameSender rtpSender("mjpeg", "127.0.0.1", 49981, 800000, 30, WIDTH, HEIGHT);
   sendingPipe += cam["out_frame"] >> rtpSender["in_data"];
-#ifdef __PROFILE__
-  TempSink tempSink;
-  sendingPipe += cam["frame_stamp"] >> tempSink["frame_stamp"];
-#endif
   std::thread sendingThread(runPipeline, &sendingPipe);
 
   debug_print("receivingPipe");
@@ -77,10 +32,6 @@ int main(int argc, char const *argv[])
   mxre::kernels::RTPFrameReceiver rtpReceiver("mjpeg", 49983, WIDTH, HEIGHT);
   mxre::kernels::CVDisplay cvDisplay;
   receivingPipe += rtpReceiver["out_data"] >> cvDisplay["in_frame"];
-#ifdef __PROFILE__
-  TempSrc tempSrc;
-  receivingPipe += tempSrc["frame_stamp"] >> cvDisplay["frame_stamp"];
-#endif
 
   std::thread recevingThread(runPipeline, &receivingPipe);
 
