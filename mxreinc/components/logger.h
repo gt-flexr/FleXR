cf027b9d165a3200e5d5dee51e7257c2350964a7
Jin Heo <993jin@gmail.com>
2021-05-10 01:20:02 -0400

    feat: add zmq_port component for mxre port

--- mxreinc/components/logger.h
+++ mxreinc/components/logger.h
@@ -5,19 +5,22 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
-namespace mxre {
-  namespace components {
-    class Logger {
-    private:
-      std::shared_ptr<spdlog::logger> instance;
-      int pid;
-    public:
-      Logger();
-      Logger(std::string loggerID, std::string logFileName);
-      void setLogger(std::string loggerID, std::string logFileName);
-      bool isSet();
-      std::shared_ptr<spdlog::logger> getInstance();
-      ~Logger();
+namespace mxre
+{
+  namespace components
+  {
+    class Logger
+    {
+      private:
+        std::shared_ptr<spdlog::logger> instance;
+        int pid;
+      public:
+        Logger();
+        Logger(std::string loggerID, std::string logFileName);
+        void setLogger(std::string loggerID, std::string logFileName);
+        bool isSet();
+        std::shared_ptr<spdlog::logger> getInstance();
+        ~Logger();
     };
   }
 }
---
+++ mxreinc/components/zmq_port.h
@@ -0,0 +1,39 @@
+#ifndef __MXRE_COMPONENTS_ZMQPORT__
+#define __MXRE_COMPONENTS_ZMQPORT__
+
+#include <zmq.hpp>
+
+namespace mxre
+{
+  namespace components
+  {
+    class ZMQPort
+    {
+      zmq::context_t ctx;
+
+      public:
+      zmq::socket_t socket;
+
+      ZMQPort() {
+        socket = zmq::socket_t(ctx, zmq::socket_type::pair);
+      }
+
+      ~ZMQPort() {
+        socket.close();
+        ctx.shutdown();
+        ctx.close();
+      }
+
+      void bind(int port) {
+        socket.bind("tcp://*:" + std::to_string(port));
+      }
+
+      void connect(std::string addr, int port) {
+        socket.connect("tcp://" + addr + ":" + std::to_string(port));
+      }
+    };
+  }
+}
+
+#endif
+
