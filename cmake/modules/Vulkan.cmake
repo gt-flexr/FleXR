31d69611c7f6789121aee91b2296e752fc266097
Shraman (Shaun) <shramanb.dev@gmail.com>
2021-10-24 21:47:38 +0000

    Refactor dependencies
    - Replace stb with stb_write_image
    - Remove stb library from vulkan_renderer
    - Add vma and renderdoc_app libraries to non-vulkan examples
    - Add C++17 build feature to non-vulkan examples

--- cmake/modules/Vulkan.cmake
+++ cmake/modules/Vulkan.cmake
@@ -16,13 +16,6 @@ FetchContent_Declare(
   GIT_PROGRESS    ON
 )
 
-FetchContent_Declare(
-  fetch_stb
-  GIT_REPOSITORY  https://github.com/nothings/stb.git
-  GIT_SHALLOW     ON
-  GIT_PROGRESS    ON
-)
-
 # TODO: Try to use GitHub links instead of raw links
 # WARNING: Requires latest cmake 3.21+ otherwise SOURCE_DIR is not populated
 FetchContent_Declare(
@@ -31,17 +24,27 @@ FetchContent_Declare(
   DOWNLOAD_NO_EXTRACT ON
 )
 
+# TODO: Try to use GitHub links instead of raw links
+# WARNING: Requires latest cmake 3.21+ otherwise SOURCE_DIR is not populated
+FetchContent_Declare(
+  fetch_stb_image_write
+  URL https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h
+  DOWNLOAD_NO_EXTRACT ON
+)
+
 FetchContent_MakeAvailable(
   fetch_vk_bootstrap
   fetch_vma
-  fetch_stb
-  fetch_renderdoc_app)
+  fetch_renderdoc_app
+  fetch_stb_image_write)
+
+# Setup header-only libraries
 
 add_library(vma INTERFACE)
 target_include_directories(vma INTERFACE ${fetch_vma_SOURCE_DIR}/src)
 
-add_library(stb INTERFACE)
-target_include_directories(stb INTERFACE ${fetch_stb_SOURCE_DIR})
-
 add_library(renderdoc_app INTERFACE)
 target_include_directories(renderdoc_app INTERFACE ${fetch_renderdoc_app_SOURCE_DIR})
+
+add_library(stb_image_write INTERFACE)
+target_include_directories(stb_image_write INTERFACE ${fetch_stb_image_write_SOURCE_DIR})
